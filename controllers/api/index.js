219f7b6c78c56c46a2597666c16ecc4e891da8b2
Chloe Harris <chloe.a.harris17@gmail.com>
2021-10-31 13:07:12 -0700

    Set up main and login handlebars files, updated login.js, added logout.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const postRoutes = require('./postRoutes');
 
+const userRoutes = require('./userRoutes');
 router.use('/users', userRoutes);
+
+const postRoutes = require('./postRoutes');
 router.use('/users', postRoutes);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 // Initialize the server
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 // Set handlebars as the template engine for the server
 const exphbs = require('express-handlebars');
